2c1a29fa228f75602c703d8031496c131a8aadfe
Manoj Sarna <codewithmanojsarna@gmail.com>
2022-04-04 22:24:32 +0530

    page:Wishlist ft. API functionality  with auth

--- src/App.jsx
+++ src/App.jsx
@@ -17,22 +17,22 @@ function App() {
       <Header />
       <ToastContainer />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/products" element={<Products />}></Route>
-        {/* <Route path="/products/:productId" element={<ProductPage />}></Route> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductPage />} />
 
         <Route element={<PrivateRoute />}>
-          <Route path="/user" element={<User />}></Route>
-          <Route path="/wishlist" element={<WishList />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/user" element={<User />} />
+          <Route path="/wishlist" element={<WishList />} />
+          <Route path="/cart" element={<Cart />} />
         </Route>
 
         <Route element={<RestrictedRoute />}>
-          <Route path="/auth" element={<Auth />}></Route>
+          <Route path="/auth" element={<Auth />} />
         </Route>
 
-        <Route path="/mockman" element={<MockAPI />}></Route>
-        <Route path="*" element={<Page404 />}></Route>
+        <Route path="/mockman" element={<MockAPI />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
       <Footer />
     </>
